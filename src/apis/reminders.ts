3dc5163d7895fd097870bf92d90d6405a966b562
S0methingSimple <30189981+S0methingSimple@users.noreply.github.com>
2023-03-20 22:00:45 +0800

    feat: add tele notif func
    Signed-off-by: S0methingSimple <30189981+S0methingSimple@users.noreply.github.com>

--- src/apis/reminders.ts
+++ src/apis/reminders.ts
@@ -12,3 +12,60 @@ export const getReminders = async () => {
   }
   return [];
 };
+
+export const taskNotification = async (type: number, chatId: number) => {
+  // let telePayload = "";
+  //      switch (taskPayload.status) {
+  //          case 2:
+  //              telePayload = `\u2757 Task ${taskPayload.taskId} (${taskPayload.location}) is overdue.`;
+  //              break;
+  //          case 3:
+  //              telePayload = `\u2705 Task ${taskPayload.taskId} (${taskPayload.location}) is completed.`;
+  //              break;
+  //          default:
+  //              return
+  //      }
+  //      axios.post(`https://api.telegram.org/bot${teleCreds.token}/sendMessage`,
+  //      {
+  //          'chat_id': teleCreds.chatId,
+  //          'text': telePayload
+  //      })
+  //      .then(() => {
+  //          console.log('Message sent');
+  //      }).catch((error) => {
+  //          console.log(error);
+  //      });
+
+}
+
+export const taskListNotification = async (chatId: number) => {
+  // let telePayload = "";
+
+  //      axios.post(`https://api.telegram.org/bot${teleCreds.token}/sendMessage`,
+  //      {
+  //          'chat_id': teleCreds.chatId,
+  //          'text': telePayload
+  //      })
+  //      .then(() => {
+  //          console.log('Message sent');
+  //      }).catch((error) => {
+  //          console.log(error);
+  //      });
+
+}
+
+export const lowBattNotification = async (chatId: number, location: number) => {
+  // let telePayload = "";
+
+  //      axios.post(`https://api.telegram.org/bot${teleCreds.token}/sendMessage`,
+  //      {
+  //          'chat_id': teleCreds.chatId,
+  //          'text': telePayload
+  //      })
+  //      .then(() => {
+  //          console.log('Message sent');
+  //      }).catch((error) => {
+  //          console.log(error);
+  //      });
+
+}
